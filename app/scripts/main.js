26a8400ede36fd0e72f576041524fe3f43509968
patsy-issa <patsy.issa@gmail.com>
2015-04-03 20:12:17 +0300

    Added sticky header

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,10 +5,40 @@
   $menu = $('#menu'),
   $close = $('.menu-close'),
   $body = $('body'),
-  $html = $('html');
+  $html = $('html'),
+  $nav = $('.header-nav'),
+  $window = $(window);
 
   $doc.on('click', '.header-nav__menu-btn, .menu-close', function() {
-    $menu.toggleClass('menu_open');
+    $menu.toggleClass('menu_open').css('top', $window.scrollTop());
     $body.toggleClass('no-scroll');
   });
+  $window.on('scroll', function() {
+    headerCheck();
+  });
+  var lastScrollTop = 0;
+
+  function headerCheck() {
+    var scrollTop = $window.scrollTop();
+    var delta = scrollTop - lastScrollTop;
+
+    lastScrollTop = scrollTop;
+
+    if (delta >= 0) {
+      // scroll down
+      if (scrollTop < $nav.height()) {
+        $nav.addClass('header-nav_is-shown');
+      } else {
+        $nav.removeClass('header-nav_is-absolute');
+      }
+      $nav.removeClass('header-nav_is-shown');
+    } else {
+      // scroll up
+      if (scrollTop === 0 || scrollTop < $nav.height()) {
+        $nav.addClass('header-nav_is-shown');
+      } else {
+        $nav.addClass('header-nav_is-shown').removeClass('header-nav_is-absolute');
+      }
+    }
+  }
 }());
